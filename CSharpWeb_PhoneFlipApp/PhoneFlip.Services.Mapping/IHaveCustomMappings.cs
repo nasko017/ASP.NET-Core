286b5e188a38a843df3084eed98f78baf0b43a19
Atanas <uluchev@abv.bg>
2024-11-25 00:27:15 +0200

    Add automapper

---
+++ CSharpWeb_PhoneFlipApp/PhoneFlip.Services.Mapping/IHaveCustomMappings.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneFlip.Services.Mapping;
+
+public interface IHaveCustomMappings
+{
+    void CreateMappings(IProfileExpression configuration);
+}
---
+++ CSharpWeb_PhoneFlipApp/PhoneFlip.Services.Mapping/IMapTo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneFlip.Services.Mapping;
+
+public interface IMapTo<T>
+{
+}
